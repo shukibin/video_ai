0ac9a62aeaf36367488fffc5032857f32bb7f1b0
shukibin <shukibin@gmail.com>
2025-01-26 10:46:57 -0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('video-ai-cache').then(cache => {
+      return cache.addAll(['/', '/index.html', '/styles.css', '/app.js', '/manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
