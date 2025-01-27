d4176be9182dd4961ed7044ce4ec90fff68f8ea4
shukibin <shukibin@gmail.com>
2025-01-26 18:36:12 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,20 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('video-ai-cache').then(cache => {
-      return cache.addAll(['/', '/index.html', '/styles.css', '/app.js', '/manifest.json']);
-    })
-  );
+const CACHE_NAME = 'video-analysis-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('fetch', (event) => {
+    event.respon
